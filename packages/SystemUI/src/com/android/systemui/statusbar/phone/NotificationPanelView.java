2b98b539c4d6af7a236654ca70cec5da317b3884
Danesh M <danesh@cyngn.com>
2016-05-12 16:24:41 -0700

    Lockscreen : Disable left edge after showing keyguard
    CYNGNOS-2816
    
    Change-Id: Ie2ef88765f52e099d61d250faa9ae4100726abf3

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1355,6 +1355,9 @@ public class NotificationPanelView extends PanelView implements
                 statusBarState == StatusBarState.SHADE_LOCKED) {
             updateDozingVisibilities(false /* animate */);
         }
+        if (statusBarState == StatusBarState.KEYGUARD) {
+            mShowingExternalKeyguard = false;
+        }
         resetVerticalPanelPosition();
         updateQsState();
     }
